488837716ca8ad2a5fed58003ed43a3f0b9f50bd
jerishey <ravinder7280846564@gmail.com>
2026-08-25 16:42:40 +0530

    Program

---
+++ JAVA/Programs/NumberList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class NumberList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
